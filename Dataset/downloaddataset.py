af76438865c6466b49749eb8d9b8099447797d7a
naominini <naonatalia25@gmail.com>
2026-05-13 22:44:00 +0300

    curatare date noi

--- Dataset/downloaddataset.py
+++ Dataset/downloaddataset.py
@@ -11,7 +11,7 @@ c.retrieve(
             'volumetric_soil_water_layer_1',
             'soil_temperature_level_1',
         ],
-        'year': ['2022'],
+        'year': [str(y) for y in range(1993, 2023)],
         'month': [
             '01','02','03','04','05','06',
             '07','08','09','10','11','12'
@@ -25,4 +25,4 @@ c.retrieve(
         'area': [38.23, 45.05, 37.12, 45.80],
         'format': 'netcdf'
     },
-    'Dataset/soil_data.nc')
+    'Dataset/soil_data_all.nc')
